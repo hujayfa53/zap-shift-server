8b24a20133ba61b1718ff591ca152e8b4e95b999
hr1971 <rifathujayfa@gmail.com>
2025-11-21 11:55:34 +0600

    create delete,post and get api

--- Index.js
+++ Index.js
@@ -2,12 +2,79 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
 const port = process.env.PORT || 4000;
 
 // middleware
 
 app.use(express.json());
 app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@firstmongoproject.tqndrnb.mongodb.net/?appName=FirstMongoProject`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db("zap_shift");
+    const parcelsCollection = db.collection("parcels");
+
+    // parcel api
+    app.get("/parcels", async (req, res) => {
+       const query = {}
+       const {email} = req.query
+
+       if (email) {
+        query.senderEmail = email;
+       }
+       const options = {sort: {createdAt: -1}}
+       const cursor = parcelsCollection.find(query,options)
+       const result = await cursor.toArray()
+       res.send(result)
+    });
+// ------------------
+    app.post("/parcels", async (req, res) => {
+      const parcel = req.body;
+      parcel.createdAt = new Date()
+      const result = await parcelsCollection.insertOne(parcel);
+      res.send(result);
+    });
+
+    // -------------------
+
+    app.get('/parcels/:id', async (req,res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await parcelsCollection.findOne(query)
+      res.send(result)
+    })
+
+    // ---------------------
+
+    app.delete('/parcels/:id', async (req,res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await parcelsCollection.deleteOne(query)
+      res.send(result)
+    })
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("zap is shifting all bangladesh");
